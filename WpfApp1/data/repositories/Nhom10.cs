855fd316cbdba87e467458d9c173859fffd0ea3a
huonggiangdhtn <99075933+huonggiangdhtn@users.noreply.github.com>
2025-03-26 08:40:58 +0700

    cập nhập nhóm 10

---
+++ WpfApp1/data/repositories/Nhom10.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesManagementApp.data.repositories
+{
+    class Nhom10
+    {
+    }
+}
