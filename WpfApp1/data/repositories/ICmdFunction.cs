350a4d5c5baab85d55d9ae4fa25cba1d28fa9f93
huonggiangdhtn <99075933+huonggiangdhtn@users.noreply.github.com>
2025-03-26 07:35:46 +0700

    them Icmd

---
+++ WpfApp1/data/repositories/ICmdFunction.cs
@@ -0,0 +1,18 @@
+﻿using SalesManagementApp.domain.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesManagementApp.data.repositories
+{
+     
+    public interface ICmdFunction
+    {
+        Task<List<User>> GetCmdFunctionsAsync();
+        Task AddCmdFunctionAsync(User user);
+        Task UpdateCmdFunctionAsync(User user);
+        Task DeleteCmdFunctionAsync(int id);
+    }
+}
